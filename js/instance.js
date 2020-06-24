351c2c86967680892c9288bd0bb219ef05af05a2
Nicola <nstorgato3@libero.it>
2020-06-24 15:10:48 +0200

    Aggiunge Parallax effect al contenuto home page
    - utilizzo libreria wagerfield/parallax

---
+++ js/instance.js
@@ -0,0 +1,5 @@
+var scene = document.getElementById('scene1');
+var parallaxInstance = new Parallax(scene);
+
+var scene = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene);
